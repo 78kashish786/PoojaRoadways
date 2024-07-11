b4d70b161d97feb96992c439b532abd8c0c4fdd1
KashishKDec6 <kashish.kataria@dec6.in>
2024-07-11 15:24:52 +0530

    reset

--- src/Components/Page_components/OurClientsSection.js
+++ src/Components/Page_components/OurClientsSection.js
@@ -4,7 +4,7 @@ const OurClientsSection = () => {
   return (
     <div className='text-center py-10'>
           <h1 className='font-md text-5xl'>Our <span className='font-light'>Clients</span></h1>
-          <hr className='border-2 border-red-600  w-[as5%] flex justify-center mx-auto mt-5'/>
+          <hr className='border-2 border-red-600  w-[5%] flex justify-center mx-auto mt-5'/>
           <div className=' text-center space-y-3 md:grid grid-cols-4 items-center  w-[70%] mx-auto my-10 gap-10'>
             <img src={require('../../Assets/Abott.png')}/>
             <img src={require('../../Assets/Indo.png')}/>
