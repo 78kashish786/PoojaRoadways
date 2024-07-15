9672f8060426572911e2493980345d6360731fef
KashishKDec6 <kashish.kataria@dec6.in>
2024-07-15 15:54:00 +0530

    header completer-m

--- src/Components/FeatureCardComponent.js
+++ src/Components/FeatureCardComponent.js
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import AOS  from 'aos';
+import "aos/dist/aos.css";
 const FeatureCardComponent = ( {image,heading, para, para2}) => {
+
+  useEffect(()=>{
+    AOS.init();
+  },[])
+
   return (
-    <div className='p-4 bg-white text-left flex flex-col justify-between items-start h-[200px] rounded-sm shadow-sm my-3 md:my-0 '>
+    <div data-aos="fade-down"data-aos-delay='300' className='p-4 bg-white text-left flex flex-col justify-between items-start h-[200px] rounded-sm shadow-sm my-3 md:my-0 '>
       <img className='w-[50px]' src={image} alt='logo images for website'/>
       <div className='space-y-1'>
       <h1 className='text-xl font-semibold '>{heading}</h1>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,59 +1,57 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-
+import React, { useEffect, useState } from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { NavLink, useLocation } from 'react-router-dom';
+import { IoMenuSharp, IoCloseSharp } from "react-icons/io5";
+  
 const Header = () => {
-  const [openMenu, setOpenMenu] = useState(false)
+  const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(()=>{
+    AOS.init();
+  },[])
 
   return (
-    <div className='md:px-24  flex justify-around items-center border sticky top-0 bg-white z-50'>
-      <img
-        className='h-[105px]'
-        src={require('../Assets/logo.png')}
-        alt='logo'
-      />
-      <ul className='hidden md:flex gap-10 uppercase text-sm'>
-        <li>
-          <NavLink to='' activeClassName='text-red-100'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/about'>About us</NavLink>
-        </li>
-        <li>
-          <NavLink to='/why-us'>Services</NavLink>
-        </li>
-        <li>
-          <NavLink to='/our-clients'>Our Clients</NavLink>
-        </li>
-        <li>
-          <NavLink to='/contact'>Contact</NavLink>
-        </li>
-      </ul>
-      {/* <div></div> */}
-      <div>
-        <ul className='hidden flex gap-10 uppercase text-sm'>
-          <li>
-            <NavLink to='' activeClassName='text-red-100'>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/about'>About us</NavLink>
-          </li>
-          <li>
-            <NavLink to='/services'>Services</NavLink>
-          </li>
-          <li>
-            <NavLink to='/our-clients'>Our Clients</NavLink>
-          </li>
-          <li>
-            <NavLink to='/contact'>Contact</NavLink>
-          </li>
-        </ul>
+    <header className="bg-white  shadow-md py-3 sticky top-0 left-0 right-0 z-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-16">
+          <div className="flex items-center">
+            <img className='w-[80px]' src={require('../Assets/logo.png')}/>
+          </div>
+          <div className="hidden md:flex md:items-center">
+            <nav className="flex space-x-4 px-10 gap-3 font-light  text-md py-10">
+              <NavLink to="/" exact activeClassName='text-red-700' className={location.pathname==='/'? "text-red-600 font-semibold":"text-gray-800"}>Home</NavLink>
+              <NavLink to="/about" exact activeClassName='text-red-700' className={location.pathname==='/about'? "text-red-600 font-semibold ":"text-gray-800"}>About </NavLink>
+              <NavLink to="/why-us" exact activeClassName='text-red-700' className={location.pathname==='/why-us'? "text-red-600 font-semibold ":"text-gray-800"}>Why us</NavLink>
+              <NavLink to="/our-clients" exact activeClassName='text-red-700' className={location.pathname==='/our-clients'? "text-red-600 font-semibold ":"text-gray-800"}>Our Clients</NavLink>
+              <NavLink to="/contact" exact activeClassName='text-red-700' className={location.pathname==='/contact'? "text-red-600 font-semibold ":"text-gray-800"}>Contact us</NavLink>
+            </nav>
+          </div>
+          <div className="md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="text-white focus:outline-none"
+            >
+              
+              {isOpen ?<IoCloseSharp size={35} className='text-red-600'/> :<IoMenuSharp size={35} className='text-red-600'/>}
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  )
-}
 
-export default Header
+      {isOpen && (
+        <div className="md:hidden "data-aos="fade-down">
+          <nav className="flex fixed top-2 w-full bg-white flex-col space-y-4 px-10 font-semibold  text-xl py-10">
+              <NavLink onClick={()=>setIsOpen(false)}  to="/" exact activeClassName='text-red-700' className={location.pathname==='/'? "text-red-600 font-semibold":"text-gray-800"}>Home</NavLink>
+              <NavLink onClick={()=>setIsOpen(false)} to="/about" exact activeClassName='text-red-700' className={location.pathname==='/about'? "text-red-600 font-semibold":"text-gray-800"}>About </NavLink>
+              <NavLink onClick={()=>setIsOpen(false)} to="/why-us" exact activeClassName='text-red-700' className={location.pathname==='/why-us'? "text-red-600 font-semibold":"text-gray-800"}>Why us</NavLink>
+              <NavLink onClick={()=>setIsOpen(false)} to="/our-clients" exact activeClassName='text-red-700' className={location.pathname==='/our-clients'? "text-red-600 font-semibold":"text-gray-800"}>Our Clients</NavLink>
+              <NavLink onClick={()=>setIsOpen(false)} to="/contact" exact activeClassName='text-red-700' className={location.pathname==='/contact'? "text-red-600 font-semibold":"text-gray-800"}>Contact us</NavLink>
+            </nav>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Header;
