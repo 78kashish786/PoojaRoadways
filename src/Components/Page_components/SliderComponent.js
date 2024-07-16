6e7d0f8695273dea8f23c58677f3aa93ee37d2a5
Kashish Kataria <61735697+78kashish786@users.noreply.github.com>
2024-07-16 13:37:14 +0530

    Update SliderComponent.js

--- src/Components/Page_components/SliderComponent.js
+++ src/Components/Page_components/SliderComponent.js
@@ -1,31 +1,49 @@
-import React from "react";
-import Slider from "react-slick";
-import SliderDiv from "./SliderDiv.js";
+import React from 'react'
+import Slider from 'react-slick'
+import SliderDiv from './SliderDiv.js'
 
-function SliderComponent() {
+function SliderComponent () {
   const settings = {
-    dots: true,
+    dots: false,
+    autoplay: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1
-  };
+  }
   return (
-    <div className="slider-container">
+    <div className='slider-container'>
       <Slider {...settings}>
         <div>
-        <SliderDiv image={require('../../Assets/website file photo edit.png')} heading={"Great At Logistics"} subheading={"this is ony the extr subheading in the componet"}/>
+          <SliderDiv
+            image={
+              'https://static.vecteezy.com/system/resources/thumbnails/027/843/401/small_2x/a-cargo-truck-with-a-container-is-seen-driving-across-a-bridge-while-a-semi-truck-with-a-cargo-trailer-follows-closely-behind-photo.jpg'
+            }
+            heading={'Best in Class Transportation Services'}
+            subheading={'Pooja Roadways are the leaders in Transport services across India'}
+          />
         </div>
         <div>
-        <SliderDiv image={require('../../Assets/website image.jpg')} heading={"Great At Logistics"} subheading={"this is ony the extr subheading ine componet"}/>
+          <SliderDiv
+            image={
+              'https://img.freepik.com/free-photo/many-transport-trucks-parked-service-station-sunset-ai-generative_123827-23416.jpg'
+            }
+            heading={'All India Transportation Services'}
+            subheading={'We Provide Services Across india with wide range of Features'}
+          />
         </div>
         <div>
-        <SliderDiv image={require('../../Assets/website poster.png')} heading={"Great At Logistics"} subheading={"this is ony the extr subheading in the componet"}/>
+          <SliderDiv
+            image={
+              'https://static.vecteezy.com/system/resources/previews/027/868/408/non_2x/a-big-truck-is-driving-along-the-highway-delivering-goods-the-concept-of-logistics-and-delivery-routes-by-road-ai-generated-free-photo.jpg'
+            }
+            heading={'Your Safety, Our Concern'}
+            subheading={'WE are concerned with the safety of your goods and so provide a safe and secure travel logistics for your goods '}
+          />
         </div>
-       
       </Slider>
     </div>
-  );
+  )
 }
 
-export default SliderComponent;
+export default SliderComponent
